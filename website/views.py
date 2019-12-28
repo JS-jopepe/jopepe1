d1b3723cab4a889ee18efe9b7f5d15c37a9e5e7d
JS-jopepe <jovenel.s@outlook.com>
2019-12-28 01:20:11 -0300

    Mexendo no static, templates e views

--- website/views.py
+++ website/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    nome = 'Jovenel'
+    lema = 'Formando um futuro melhor'
+    lista_de_cursos = [
+        'Inglês',
+        'Francês',
+        'Informática',
+        'Português',
+    ]
+
+    context = {
+        'nome' : nome
+        'lema' : lema
+        'cursos' : lista_de_cursos
+    }
+
+    return render(request, 'home.html', context)
+
